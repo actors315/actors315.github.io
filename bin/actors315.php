90678a3817a0271b27159ccd4a4cffaf99aafc64
actors315 <actors315@gmail.com>
2019-01-30 00:42:09 +0800

    元数据

--- bin/actors315.php
+++ bin/actors315.php
@@ -121,7 +121,7 @@ array_multisort($published, SORT_DESC, $list);
 $file = __DIR__ . "/../blog/index.md";
 file_put_contents($file, '---  '.PHP_EOL);
 file_put_contents($file, 'layout: list ', FILE_APPEND);
-file_put_contents($file, 'title: 我的博客 ', FILE_APPEND);
+file_put_contents($file, 'title: 我的博客 '.PHP_EOL, FILE_APPEND);
 file_put_contents($file, '---  '.PHP_EOL, FILE_APPEND);
 
 file_put_contents($file, PHP_EOL . PHP_EOL, FILE_APPEND);
